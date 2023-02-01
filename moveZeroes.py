c3237fef0e42a1d3e52eee7a2d85a18972331a2f
Nate Trichanh <natetrichanh@gmail.com>
2023-01-31 20:02:06 -0800

    Move Zeroes Leetcode Easy 283

---
+++ moveZeroes.py
@@ -0,0 +1,11 @@
+def moveZeroes(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: None Do not return anything, modify nums in-place instead.
+        """
+        left = 0
+        for right in range(len(nums)):
+            if nums[right]:
+                nums[left], nums[right] = nums[right], nums[left]
+                left += 1
+        return nums
