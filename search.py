726e9c5c6cee290d30e85774c5f822b775206e2d
Nate Trichanh <natetrichanh@gmail.com>
2023-02-01 23:11:00 -0800

    Binary Search Updated (more efficient)

--- search.py
+++ search.py
@@ -4,15 +4,28 @@ def search(self, nums, target):
         :type target: int
         :rtype: int
         """
-        start = 0
-        end = len(nums) - 1
-        while start < end:
-            midpoint = (start + end) // 2
+        # start = 0
+        # end = len(nums) - 1
+        # while start < end:
+        #     midpoint = (start + end) // 2
+        #     midpoint_value = nums[midpoint]
+        #     if midpoint_value == target:
+        #         return midpoint
+        #     elif target < midpoint_value:
+        #         end = midpoint - 1
+        #     elif target > midpoint_value:
+        #         start = midpoint + 1
+        # return -1
+
+        # More efficient solution
+        left, right = 0, len(nums) - 1
+        while left <= right:
+            midpoint = left + (right - left) // 2
             midpoint_value = nums[midpoint]
-            if midpoint_value == target:
+            if midpoint_value > target:
+                right = midpoint - 1
+            elif midpoint_value < target: 
+                left = midpoint + 1
+            else:
                 return midpoint
-            elif target < midpoint_value:
-                end = midpoint - 1
-            elif target > midpoint_value:
-                start = midpoint + 1
         return -1
