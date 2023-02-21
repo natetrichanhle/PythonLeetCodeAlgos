758aab24dcd5e4d7a8ded155734583edffb7081a
Nate Trichanh <natetrichanh@gmail.com>
2023-02-20 21:42:12 -0800

    Kth Largest Element in an Array Leetcode 215

---
+++ findKthLargest.py
@@ -0,0 +1,16 @@
+def findKthLargest(self, nums: List[int], k: int) -> int:
+        def quickSort(nums):
+            if len(nums) <= 1: return nums
+
+            pivot = random.choice(nums)
+
+            less_than, equal_to, greater_than = [], [], []
+
+            for val in nums:
+                if val < pivot: less_than.append(val)
+                elif val > pivot: greater_than.append(val)
+                else: equal_to.append(val)
+
+            return quickSort(less_than) + equal_to + quickSort(greater_than)
+        sortedArr = quickSort(nums)
+        return sortedArr[-k]
