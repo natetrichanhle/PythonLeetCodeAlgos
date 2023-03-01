a2f58ffbab6813514d57c46835c330ff474da7ed
Nate Trichanh <natetrichanh@gmail.com>
2023-02-28 22:55:42 -0800

    278. First Bad Version

--- firstBadVersion.py
+++ firstBadVersion.py
@@ -1,15 +1,32 @@
-def firstBadVersion(self, n):
-        """
-        :type n: int
-        :rtype: int
-        """
-        # [1,2,3,4,5]
-        start = 1
-        end = n
-        while start < end:
-            mid = (start + end) // 2
+# def firstBadVersion(self, n):
+#         """
+#         :type n: int
+#         :rtype: int
+#         """
+#         # [1,2,3,4,5]
+#         start = 1
+#         end = n
+#         while start < end:
+#             mid = (start + end) // 2
+#             if isBadVersion(mid):
+#                 end = mid
+#             else:
+#                 start = mid + 1
+#         return start
+
+# The isBadVersion API is already defined for you.
+# def isBadVersion(version: int) -> bool:
+
+class Solution:
+    def firstBadVersion(self, n: int) -> int:
+        # binary search
+        low, high = 1, n
+        while low < high:
+            mid = low + (high - low) // 2
             if isBadVersion(mid):
-                end = mid
+                high = mid
             else:
-                start = mid + 1
-        return start
+                # this has to be + 1 because we want to find the first bad version not the last good version
+                low = mid + 1
+        return low
+            
