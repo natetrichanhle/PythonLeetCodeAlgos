b1909d1e45bd910193e289ae355935d36a31a8ea
Nate Trichanh <natetrichanh@gmail.com>
2023-04-12 13:54:15 -0700

    Leetcode 26 second attempt

--- removeDuplicates.py
+++ removeDuplicates.py
@@ -2,11 +2,26 @@ class Solution:
     def removeDuplicates(self, nums: List[int]) -> int:
         # since it's sorted we know that duplicates are next to each other
         # two pointers - one that traverses until it is a diff number and one that stays for the comparison
-        
+
         left = 1
 
         for right in range(1, len(nums)):
             if nums[right] != nums[right - 1]:
                 nums[left] = nums[right]
                 left += 1
-        return left       
+        return left
+
+    class Solution:
+
+    def removeDuplicates(self, nums: List[int]) -> int:
+        # both pointers start at index 1 since we know that index 0 is already sorted
+        L = 1
+        for R in range(1, len(nums)):
+            # if the number at the right pointer compared to the number at the index before it is different, then we know it's the first time we've seen it
+            if nums[R] != nums[R - 1]:
+                # we then want to make the left pointers value equal to the value of right pointer where we discovered the new unique value
+                nums[L] = nums[R]
+                # after we do all the procedures before this, we increment the left pointer
+                L += 1
+        # return L which at the end of the iterations, is the amount of unqiue values
+        return L
